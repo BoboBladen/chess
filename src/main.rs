b3e0c8099fb9066b09a5cac31abbb02bfb03c292
Hugo Fryklin <hugo.fryklin@gmail.com>
2024-10-13 16:31:28 +0200

    Show unicode chess symbols and change some rendering

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use glutin_window::GlutinWindow;
 use std::cmp;
 
-use opengl_graphics::{GlGraphics, OpenGL};
+use opengl_graphics::{GlGraphics, GlyphCache, OpenGL, TextureSettings};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{Button, MouseButton};
 use piston::{EventLoop, RenderEvent, WindowSettings};
@@ -35,6 +35,11 @@ fn main() {
     let mut event = Events::new(EventSettings::new().lazy(false));
     let mut gl = GlGraphics::new(opengl);
     let mut mouse_position: [f64; 2] = [0.0, 0.0];
+    // import the font
+    let font = "fonts/DejaVuSansCondensed-Bold.ttf";
+    // Create a piston specific 'Glyphs' from the font
+    let mut glyphs =
+        GlyphCache::new(font, (), TextureSettings::new()).expect("Could not load font");
 
     while let Some(e) = event.next(&mut window) {
         if let Some(position) = e.mouse_cursor_args() {
@@ -73,7 +78,7 @@ fn main() {
                 use graphics::clear;
                 clear([1.0; 4], g);
                 // draw board
-                render::draw_board(&c, g, theboard.clone());
+                render::draw_board(&c, g, theboard.clone(), &mut glyphs);
             });
         }
     }
--- src/render.rs
+++ src/render.rs
@@ -5,14 +5,16 @@ use opengl_graphics::*;
 pub use crate::lib::Board;
 use crate::SCREEN_HEIGHT;
 
-const COLOR1: [f32; 4] = [0.4, 0.35, 0.35, 1.0];
+const COLOR1: [f32; 4] = [0.45, 0.4, 0.4, 1.0];
 const COLOR2: [f32; 4] = [0.9, 0.8, 0.75, 1.0];
 const CELL_SIZE: usize = SCREEN_HEIGHT / 8;
 
-pub fn draw_board<G: Graphics<Texture = Texture>>(c: &Context, g: &mut G, board: Board) {
-    let mut transform = c
-        .transform
-        .trans((CELL_SIZE / 2) as f64, (CELL_SIZE / 2) as f64);
+pub fn draw_board<G: Graphics<Texture = Texture>>(
+    c: &Context,
+    g: &mut G,
+    board: Board,
+    glyphs: &mut GlyphCache,
+) {
     let mut cnt = 0;
     for i in 0..64 {
         let mut color: [f32; 4] = COLOR1;
@@ -43,28 +45,39 @@ pub fn draw_board<G: Graphics<Texture = Texture>>(c: &Context, g: &mut G, board:
         );
 
         if let Some(piece) = &board.board[i] {
-            let name: &str = &piece.name.to_string();
+            let name;
+            match piece.name.to_string().as_str() {
+                // White pieces
+                "P" => name = '\u{2659}', // White Pawn
+                "R" => name = '\u{2656}', // White Rook
+                "N" => name = '\u{2658}', // White Knight
+                "B" => name = '\u{2657}', // White Bishop
+                "Q" => name = '\u{2655}', // White Queen
+                "K" => name = '\u{2654}', // White King
 
-            // import the font
-            let font = "fonts/OpenSans-Regular.ttf";
-            // Create a piston specific 'Glyphs' from the font
-            let mut glyphs =
-                GlyphCache::new(font, (), TextureSettings::new()).expect("Could not load font");
+                // Black pieces
+                "p" => name = '\u{265F}', // Black Pawn
+                "r" => name = '\u{265C}', // Black Rook
+                "n" => name = '\u{265E}', // Black Knight
+                "b" => name = '\u{265D}', // Black Bishop
+                "q" => name = '\u{265B}', // Black Queen
+                "k" => name = '\u{265A}', // Black King
+                _ => name = ' ',
+            }
+            let piece_transform = c.transform.trans(
+                x as f64 + CELL_SIZE as f64 / 2.0 - 22.0, // 22 is about half the width of the character
+                y as f64 + CELL_SIZE as f64 / 2.0 + 22.0,
+            );
             // A transform to move the placement of the text
-            graphics::text::Text::new(32)
-                .draw(name, &mut glyphs, &c.draw_state, transform, g)
+            graphics::text::Text::new(50)
+                .draw(&name.to_string(), glyphs, &c.draw_state, piece_transform, g)
                 .unwrap();
 
             // For some reason this is needed for the text to render correctly
             // draws a space in another color
             graphics::text::Text::new_color([1.0; 4], 32)
-                .draw(" ", &mut glyphs, &c.draw_state, c.transform, g)
+                .draw(" ", glyphs, &c.draw_state, c.transform, g)
                 .unwrap();
-        }
-        if i % 8 == 7 {
-            transform = transform.trans((-7 * CELL_SIZE as i32) as f64, CELL_SIZE as f64);
-        } else {
-            transform = transform.trans((CELL_SIZE) as f64, 0.0);
         }
     }
 }
