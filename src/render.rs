43e13ed528835c5947f2e24b22917e0dd47086ba
Hugo Fryklin <hugo.fryklin@gmail.com>
2024-10-14 13:17:19 +0200

    add nicer visuals

--- src/render.rs
+++ src/render.rs
@@ -1,5 +1,7 @@
+use graphics::{ellipse, Ellipse};
 use graphics::{Context, Graphics};
 use graphics::{Rectangle, Transformed};
+
 use opengl_graphics::*;
 
 pub use crate::lib::Board;
@@ -21,21 +23,28 @@ pub fn draw_board<G: Graphics<Texture = Texture>>(
         if (i + cnt) % 2 == 0 {
             color = COLOR2;
         }
-        if let Some(sq) = board.selected {
-            if let Some(_sq2) = &board.board[sq] {
-                if board.get_valid_moves(sq).contains(&i) {
-                    color = [0.7, 0.7, 0.7, 1.0];
-                }
-            }
-            if sq == i {
-                color = [0.6, 0.3, 0.3, 1.0];
-            }
-        }
         if (i % 8) == 7 {
             cnt += 1;
         }
         let x = (i % 8) * CELL_SIZE;
         let y = (i / 8) * CELL_SIZE;
+
+        let mut draw_circle = false;
+        let mut circle_color = [0.7, 0.7, 0.7, 1.0];
+
+        if let Some(sq) = board.selected {
+            if let Some(_sq2) = &board.board[sq] {
+                if board.get_valid_moves(sq).contains(&i) {
+                    if let Some(_p) = &board.board[i] {
+                        circle_color = [0.6, 0.3, 0.3, 1.0];
+                    }
+                    draw_circle = true;
+                }
+            }
+            if sq == i {
+                color = [0.3, 0.6, 0.3, 1.0];
+            }
+        }
 
         Rectangle::new(color).draw(
             [x as f64, y as f64, CELL_SIZE as f64, CELL_SIZE as f64],
@@ -65,8 +74,8 @@ pub fn draw_board<G: Graphics<Texture = Texture>>(
                 _ => name = ' ',
             }
             let piece_transform = c.transform.trans(
-                x as f64 + CELL_SIZE as f64 / 2.0 - 22.0, // 22 is about half the width of the character
-                y as f64 + CELL_SIZE as f64 / 2.0 + 22.0,
+                x as f64 + CELL_SIZE as f64 / 2.0 - 24.0, // 24 is about half the width of the character
+                y as f64 + CELL_SIZE as f64 / 2.0 + 24.0,
             );
             // A transform to move the placement of the text
             graphics::text::Text::new(50)
@@ -78,6 +87,21 @@ pub fn draw_board<G: Graphics<Texture = Texture>>(
             graphics::text::Text::new_color([1.0; 4], 32)
                 .draw(" ", glyphs, &c.draw_state, c.transform, g)
                 .unwrap();
+        }
+
+        //draw the circle on top of everything else
+        if draw_circle {
+            Ellipse::new(circle_color).draw(
+                [
+                    (x + CELL_SIZE / 3) as f64,
+                    (y + CELL_SIZE / 3) as f64,
+                    (CELL_SIZE / 3) as f64,
+                    (CELL_SIZE / 3) as f64,
+                ],
+                &c.draw_state,
+                c.transform,
+                g,
+            );
         }
     }
 }
